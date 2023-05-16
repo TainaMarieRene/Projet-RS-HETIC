990736ebf273e6131d64e5fbdba47418d7da9e0e
AlessGarau <alessandrog.pro@gmail.com>
2023-05-16 22:02:10 +0200

    fix: function getUser

--- Controllers/functions.php
+++ Controllers/functions.php
@@ -12,35 +12,35 @@ function db()
     return new PDO($dsn, "$user", "$password", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'));
 }
 
-function getUserPoser(int $userId)
-{
-
+function getUserPosts(int $userId){
     $pdo = db();
+    $postContent = null;
+    
     $requete = $pdo->prepare("SELECT
-    p.post_id,
-    p.post_content,
-    pc.post_comment_id,
-    pc.post_comment_content,
-    pcc.post_comment_id AS child_comment_id,
-    pcc.post_comment_content AS child_comment_content,
-    r.reaction_type,
-    r.reaction_type_id,
-    r.user_id,
-    r.reaction_emoji
-FROM
-    friends AS f
-    INNER JOIN posts AS p ON (f.user_id2 = p.user_id)
-    LEFT JOIN posts_comments AS pc ON (p.post_id = pc.post_id)
-    LEFT JOIN posts_comments AS pcc ON (pc.post_comment_id = pcc.post_comment_parent_id)
-    LEFT JOIN reactions AS r ON (p.post_id = r.reaction_type_id AND r.reaction_type = 'post' AND r.user_id = 1)
-WHERE
-    f.user_id1 = 1
-ORDER BY
-    p.post_id, pc.post_comment_id, pcc.post_comment_id;
+        p.post_id,
+        p.post_content,
+        pc.post_comment_id,
+        pc.post_comment_content,
+        pcc.post_comment_id AS child_comment_id,
+        pcc.post_comment_content AS child_comment_content,
+        r.reaction_type,
+        r.reaction_type_id,
+        r.user_id,
+        r.reaction_emoji
+    FROM
+        friends AS f
+        INNER JOIN posts AS p ON f.user_id2 = p.user_id
+        LEFT JOIN posts_comments AS pc ON p.post_id = pc.post_id
+        LEFT JOIN posts_comments AS pcc ON pc.post_comment_id = pcc.post_comment_parent_id
+        LEFT JOIN reactions AS r ON p.post_id = r.reaction_type_id AND r.reaction_type = 'post' AND r.user_id = :user_id
+    WHERE
+        f.user_id1 = :user_id
+    ORDER BY
+        p.post_id, pc.post_comment_id, pcc.post_comment_id");
 
-");
     $requete->execute([
-        ':user_id' => $userId
+        ':user_id' => $userId,
+        ':post_content' => $postContent
     ]);
 
     $posts = $requete->fetchAll(PDO::FETCH_ASSOC);
--- Views/feed.php
+++ Views/feed.php
@@ -1,7 +1,7 @@
 <?php 
-//require_once("../controllers/functions.php");
-// $post=getUserPoser(1);
-// var_dump($post);
+require_once("../controllers/functions.php");
+$post = getUserPosts(1); // Utilise getUserPosts au lieu de getUserPoser
+var_dump($post);
 ?>
 
 <!DOCTYPE html>
@@ -10,17 +10,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feed - <?="TON NOM"?></title>
+    <title>Feed - <?php echo "TON NOM"; ?></title>
 </head>
 <body>
-    <?php require_once("./templates/side_profile.php")?>
+    <?php require_once("./templates/side_profile.php"); ?>
     <article>
         <img src="<?= '' ?>" alt="friend_pp" />
-        <span><?="friend_pseudo"?><span> 
-        <p><?="post_date"?></p>
-        <p><?="post_content"?></p>
-        <img src="" alt="coeur">
-        <img src="" alt="commentary">
+        <span><?php echo "friend_pseudo"; ?><span> 
+        <p><?php echo "post_date"; ?></p>
+        <p><?php echo "post_content"; ?></p>
+        <img src="./assets/icons/commentary.svg" alt="commentary">
+        <img src="./assets/icons/like.svg" alt="coeur">
+        <span class="like"><?php echo "likes"; ?></span>
+        <span class="commentaire"><?php echo "nombre de commentaire"; ?></span>
     </article>
 </body>
 </html>
