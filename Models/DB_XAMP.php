fdf49eaa200b722ddff5f9d0e166e0b688e4f51f
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-16 14:57:53 +0200

    feat: xamp db class

---
+++ Models/DB_XAMP.php
@@ -0,0 +1,29 @@
+<?php
+
+class DBXAMP
+{
+    private string $host = "localhost";
+    private string $user = "root";
+    private string $password = "";
+    private string $db_name = "unilink";
+    public PDO $status;
+
+    public function __construct(){
+        try {
+            $dsn = "mysql:host=$this->host;dbname=$this->db_name";
+            $this->state = new PDO($dsn,"$this->user","$this->password",array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'));
+            $this->state->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->state->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch(PDOException $e) {
+            http_response_code(500);
+            echo json_encode([
+                "code" => $e->getCode(),
+                "message" => $e->getMessage(),
+                "file" => $e->getFile(),
+                "line" => $e->getLine()
+            ]);
+            exit;
+        }
+    }
+
+}
