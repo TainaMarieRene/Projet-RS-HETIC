389ac545f9e157764bd47c1119ad802cc96203b4
LeBenjos <benjamin.schinkel01@outlook.fr>
2023-05-16 14:12:23 +0200

    add: db sql

---
+++ +MD_Assets/UniLink.sql
@@ -0,0 +1,516 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: May 16, 2023 at 12:08 PM
+-- Server version: 5.7.34
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `UniLink`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authentifications`
+--
+
+CREATE TABLE `authentifications` (
+  `user_id` int(11) NOT NULL,
+  `user_username` varchar(255) NOT NULL,
+  `user_token` varchar(255) NOT NULL,
+  `user_token_start` datetime NOT NULL,
+  `user_token_end` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conversations`
+--
+
+CREATE TABLE `conversations` (
+  `conversation_id` int(11) NOT NULL,
+  `conversation_name` varchar(255) NOT NULL,
+  `conversation_picture` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conversations_members`
+--
+
+CREATE TABLE `conversations_members` (
+  `conversation_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends`
+--
+
+CREATE TABLE `friends` (
+  `user_id1` int(11) NOT NULL,
+  `user_id2` int(11) NOT NULL,
+  `user_relation` enum('waiting','blocked','friend') NOT NULL DEFAULT 'waiting'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `group_id` int(11) NOT NULL,
+  `group_status` tinyint(1) NOT NULL DEFAULT '0',
+  `group_creator` int(11) NOT NULL,
+  `group_at` varchar(255) NOT NULL,
+  `group_name` varchar(255) NOT NULL,
+  `group_picture` varchar(255) DEFAULT NULL,
+  `group_banner` varchar(255) DEFAULT NULL,
+  `group_desc` varchar(255) DEFAULT NULL,
+  `group_tag` set('tech','web','design','3D') DEFAULT NULL,
+  `group_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `member_id` int(11) NOT NULL,
+  `member_type` enum('group','page') NOT NULL,
+  `member_type_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members_groups_extra`
+--
+
+CREATE TABLE `members_groups_extra` (
+  `member_id` int(11) NOT NULL,
+  `member_group_request` enum('waiting','blocked','accept') NOT NULL DEFAULT 'waiting',
+  `member_group_role` enum('admin','member','creator') NOT NULL DEFAULT 'member'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `message_id` int(11) NOT NULL,
+  `conversation_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message_parent_id` int(11) DEFAULT NULL,
+  `message_content` text NOT NULL,
+  `message_status` enum('send','see') NOT NULL DEFAULT 'send',
+  `message_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages_imgs`
+--
+
+CREATE TABLE `messages_imgs` (
+  `message_id` int(11) NOT NULL,
+  `message_img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `page_id` int(11) NOT NULL,
+  `page_creator` int(11) NOT NULL,
+  `page_at` varchar(255) NOT NULL,
+  `page_name` varchar(255) NOT NULL,
+  `page_picture` varchar(255) DEFAULT NULL,
+  `page_banner` varchar(255) DEFAULT NULL,
+  `page_desc` varchar(255) DEFAULT NULL,
+  `page_tag` set('tech','web','design','3D') DEFAULT NULL,
+  `page_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `page_certification` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_type` enum('group','page','profile') NOT NULL,
+  `post_type_id` int(11) NOT NULL,
+  `post_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts_comments`
+--
+
+CREATE TABLE `posts_comments` (
+  `post_comment_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_comment_parent_id` int(11) DEFAULT NULL,
+  `post_comment_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts_imgs`
+--
+
+CREATE TABLE `posts_imgs` (
+  `post_id` int(11) NOT NULL,
+  `post_img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `user_id` int(11) NOT NULL,
+  `profile_picture` varchar(255) NOT NULL,
+  `profile_banner` varchar(255) NOT NULL,
+  `profile_bio` varchar(255) DEFAULT NULL,
+  `profile_location` varchar(255) DEFAULT NULL,
+  `profile_activity` varchar(255) DEFAULT NULL,
+  `profile_certification` tinyint(1) NOT NULL DEFAULT '0',
+  `profile_status` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reactions`
+--
+
+CREATE TABLE `reactions` (
+  `reaction_type` enum('group','page','profil') NOT NULL,
+  `reaction_type_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `reaction_emoji` enum('react1','react2','react3','react4','react5') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_username` varchar(255) NOT NULL,
+  `user_mail` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_firstname` varchar(255) NOT NULL,
+  `user_lastname` varchar(255) NOT NULL,
+  `user_birthdate` date NOT NULL,
+  `user_account_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_account_status` enum('disable','waiting','valid') NOT NULL DEFAULT 'waiting'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `authentifications`
+--
+ALTER TABLE `authentifications`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_username` (`user_username`),
+  ADD UNIQUE KEY `user_token` (`user_token`);
+
+--
+-- Indexes for table `conversations`
+--
+ALTER TABLE `conversations`
+  ADD PRIMARY KEY (`conversation_id`);
+
+--
+-- Indexes for table `conversations_members`
+--
+ALTER TABLE `conversations_members`
+  ADD PRIMARY KEY (`conversation_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`user_id1`,`user_id2`),
+  ADD KEY `user_id2` (`user_id2`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`group_id`),
+  ADD UNIQUE KEY `group_at` (`group_at`),
+  ADD KEY `group_creator` (`group_creator`);
+
+--
+-- Indexes for table `members`
+--
+ALTER TABLE `members`
+  ADD PRIMARY KEY (`member_id`),
+  ADD UNIQUE KEY `member_unique` (`user_id`,`member_type`,`member_type_id`);
+
+--
+-- Indexes for table `members_groups_extra`
+--
+ALTER TABLE `members_groups_extra`
+  ADD PRIMARY KEY (`member_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`message_id`),
+  ADD KEY `conversation_id` (`conversation_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `messages_imgs`
+--
+ALTER TABLE `messages_imgs`
+  ADD PRIMARY KEY (`message_id`,`message_img`);
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`page_id`),
+  ADD UNIQUE KEY `page_at` (`page_at`),
+  ADD KEY `page_creator` (`page_creator`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `posts_comments`
+--
+ALTER TABLE `posts_comments`
+  ADD PRIMARY KEY (`post_comment_id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `posts_imgs`
+--
+ALTER TABLE `posts_imgs`
+  ADD PRIMARY KEY (`post_id`,`post_img`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `reactions`
+--
+ALTER TABLE `reactions`
+  ADD PRIMARY KEY (`reaction_type`,`reaction_type_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_username` (`user_username`),
+  ADD UNIQUE KEY `user_mail` (`user_mail`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `conversations`
+--
+ALTER TABLE `conversations`
+  MODIFY `conversation_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `members`
+--
+ALTER TABLE `members`
+  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `page_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts_comments`
+--
+ALTER TABLE `posts_comments`
+  MODIFY `post_comment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `authentifications`
+--
+ALTER TABLE `authentifications`
+  ADD CONSTRAINT `authentifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `authentifications_ibfk_2` FOREIGN KEY (`user_username`) REFERENCES `users` (`user_username`);
+
+--
+-- Constraints for table `conversations_members`
+--
+ALTER TABLE `conversations_members`
+  ADD CONSTRAINT `conversations_members_ibfk_1` FOREIGN KEY (`conversation_id`) REFERENCES `conversations` (`conversation_id`),
+  ADD CONSTRAINT `conversations_members_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `friends`
+--
+ALTER TABLE `friends`
+  ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`user_id1`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`user_id2`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `groups`
+--
+ALTER TABLE `groups`
+  ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`group_creator`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `members`
+--
+ALTER TABLE `members`
+  ADD CONSTRAINT `members_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `members_groups_extra`
+--
+ALTER TABLE `members_groups_extra`
+  ADD CONSTRAINT `members_groups_extra_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`);
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`conversation_id`) REFERENCES `conversations` (`conversation_id`),
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `messages_imgs`
+--
+ALTER TABLE `messages_imgs`
+  ADD CONSTRAINT `messages_imgs_ibfk_1` FOREIGN KEY (`message_id`) REFERENCES `messages` (`message_id`);
+
+--
+-- Constraints for table `pages`
+--
+ALTER TABLE `pages`
+  ADD CONSTRAINT `pages_ibfk_1` FOREIGN KEY (`page_creator`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `posts_comments`
+--
+ALTER TABLE `posts_comments`
+  ADD CONSTRAINT `posts_comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`),
+  ADD CONSTRAINT `posts_comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `posts_imgs`
+--
+ALTER TABLE `posts_imgs`
+  ADD CONSTRAINT `posts_imgs_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`);
+
+--
+-- Constraints for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `reactions`
+--
+ALTER TABLE `reactions`
+  ADD CONSTRAINT `reactions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
