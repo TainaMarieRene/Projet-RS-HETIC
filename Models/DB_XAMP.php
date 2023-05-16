c28a8229b2c22d434f1f310b4711116b5a538d4e
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-16 15:31:00 +0200

    fix: db xamp return statement

--- Models/DB_XAMP.php
+++ Models/DB_XAMP.php
@@ -15,14 +15,7 @@ class DBXAMP
             $this->state->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->state->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch(PDOException $e) {
-            http_response_code(500);
-            echo json_encode([
-                "code" => $e->getCode(),
-                "message" => $e->getMessage(),
-                "file" => $e->getFile(),
-                "line" => $e->getLine()
-            ]);
-            exit();
+            return $e;
         }
     }
 
