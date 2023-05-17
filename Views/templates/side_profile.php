dfcce34a6cb10fd0b7437b60af8781442b911c39
AlessGarau <alessandrog.pro@gmail.com>
2023-05-17 16:04:51 +0200

    feat: css sideProfile

--- Views/templates/side_profile.php
+++ Views/templates/side_profile.php
@@ -1,24 +1,31 @@
 <head>
-
+    <link rel="stylesheet" href="../styles/sideProfile.css">
 </head>
 
 <aside>
-    <img src="<?= '' ?>" alt="PP" />
-    <span>
-        <?= "Pseudo" ?><span>
-            <p>
-                <?= "La bio" ?>
-            </p>
-            <nav>
-                <ul>
-                    <li> <a href="http://">Home</a></li>
-                    <li> <a href="http://">Notifications</a></li>
-                    <li> <a href="http://">Friends</a></li>
-                    <li> <a href="http://">Pages</a></li>
-                    <li> <a href="http://">Groups</a></li>
-                    <li><a href="http://">Settings</a></li>
-                    <li> <a href="http://">Logout</a></li>
-                </ul>
-            </nav>
+    <div class="profile">
+        <img src="<?= "https://www.liberation.fr/resizer/4aGj4IanoRaHCqrPJx-tLrl0uYY=/1024x0/filters:format(jpg):quality(70):focal(2395x2721:2405x2731)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/2GMZBJOWNNBNVHMMTL3XJOIV7I.jpg" ?>"
+            alt="PP" />
+        <span>
+            <?= "@Pseudo" ?>
+            <span>
+    </div>
+    <div class="bio">
+        <p>
+            <?= "bio: Je vis que pour faire plus de papier" ?>
+        </p>
+    </div>
+
+    <nav>
+        <ul>
+            <li> <a href="http://">Home</a></li>
+            <li> <a href="http://">Notifications</a></li>
+            <li> <a href="http://">Friends</a></li>
+            <li> <a href="http://">Pages</a></li>
+            <li> <a href="http://">Groups</a></li>
+            <li><a href="http://">Settings</a></li>
+            <li> <a href="http://">Logout</a></li>
+        </ul>
+    </nav>
 
 </aside>
