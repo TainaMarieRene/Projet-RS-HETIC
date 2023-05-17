3e604851a35391fc36be7a7eacbe2304a96d8d35
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-17 10:11:36 +0200

    feat: header semantics

---
+++ Views/templates/header.php
@@ -0,0 +1,18 @@
+<?php
+$username = ""
+
+?>
+
+<header>
+    <h1>Uni</h1>
+    <div class="inputSearch">
+        <label>
+            <img src="../assets/icons/searchIcon.svg">
+            <input type="search" placeholder="Chercher un membre">
+        </label>
+    </div>
+    <nav class="headerNav">
+        <a href="#" class="headerCTA">Create</a>
+        <img src="../assets/imgs/users/picture/<?= $username ?>">
+    </nav>
+</header>
