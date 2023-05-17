a574cca9e4c83bafc9f03df4557aca7e07003a64
AlessGarau <alessandrog.pro@gmail.com>
2023-05-17 11:10:26 +0200

    fix: semantic side_profile

--- Views/templates/side_profile.php
+++ Views/templates/side_profile.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
 
-<body>
     <aside>
         <img src="<?= '' ?>" alt="PP" />
         <span><?="Pseudo"?><span> 
@@ -24,6 +19,3 @@
             <li> <a href="http://">Logout</a></li>
         </ul>
     </aside>
-</body>
-
-</html>
