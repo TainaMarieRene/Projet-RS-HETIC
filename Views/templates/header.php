4cca0aed83c2fa0a62b163d1698db2e70cc98466
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-17 10:57:49 +0200

    fix: add alt to imgs

--- Views/templates/header.php
+++ Views/templates/header.php
@@ -10,12 +10,12 @@ $username = ""
     <h1>Uni</h1>
     <div class="inputSearch">
         <label>
-            <img src="./assets/icons/searchIcon.svg">
+            <img src="./assets/icons/searchIcon.svg" alt="Icone de recherche">
             <input type="search" placeholder="Chercher un membre">
         </label>
     </div>
     <nav class="headerNav">
         <a href="#" class="headerCTA">Create</a>
-        <img src="../assets/imgs/users/picture/<?= $username ?>">
+        <img src="../assets/imgs/users/picture/<?= $username ?>" alt="Image de l'utilisateur">
     </nav>
 </header>
