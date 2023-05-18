80572e4088b8d9c30952d1eeefcf8fc92e6a37a4
LeBenjos <benjamin.schinkel01@outlook.fr>
2023-05-18 03:35:13 +0200

    add: register regex

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -3,15 +3,57 @@
 namespace AuthController;
 
 class LoginController {
-    public function __construct(){
+    private string $_page;
+    private string $_method;
+    
+    public function __construct($page, $method){
+        $this->_page = $page;
+        $this->_method = $method;
         require '/Applications/MAMP/htdocs/Projet-RS-HETIC/Models/Users.php';
         require '/Applications/MAMP/htdocs/Projet-RS-HETIC/Views/login.php';
     }
 }
 
 class RegisterController {
-    public function __construct(){
+    private string $_page;
+    private string $_method;
+
+    public function __construct($page, $method){
+        $this->_page = $page;
+        $this->_method = $method;
+        
+        switch ($this->_method){
+            case "GET" :
+                break;
+            case "POST" :
+                $firstname = preg_match("`^((?:(?:[a-zA-ZáâàãçéêèëïíóôõúüÁÂÀÃÇÉÈÊËÏÍÓÔÕÚÜ]+)(?:-(?:[a-zA-ZáâàãçéêèëïíóôõúüÁÂÀÃÇÉÈÊËÏÍÓÔÕÚÜ]+))+)|(?:[a-zA-ZáâàãçéêèëïíóôõúüÁÂÀÃÇÉÈÊËÏÍÓÔÕÚÜ]+))$`", filter_input(INPUT_POST, "firstname")) ? filter_input(INPUT_POST, "firstname") : false ;
+                $lastname = preg_match("`^((?:(?:[a-zA-ZáâàãçéêèëïíóôõúüÁÂÀÃÇÉÈÊËÏÍÓÔÕÚÜ]+)(?:-(?:[a-zA-ZáâàãçéêèëïíóôõúüÁÂÀÃÇÉÈÊËÏÍÓÔÕÚÜ]+))+)|(?:[a-zA-ZáâàãçéêèëïíóôõúüÁÂÀÃÇÉÈÊËÏÍÓÔÕÚÜ]+))$`", filter_input(INPUT_POST, "lastname")) ? filter_input(INPUT_POST, "lastname") : false ;
+                
+                $birthdate = preg_match("`^([0-9]{4})(-)(0[1-9]|1[0-2])(-)(0[1-9]|1[0-9]|2[0-9]|3[0-1])$`", filter_input(INPUT_POST, "birthdate")) ? filter_input(INPUT_POST, "birthdate") : false;
+                $birthdate = ($this->checkYears($birthdate)) ? $birthdate : false;
+
+                $username = preg_match("`^(?=.{3,20}$)(?![_.-])(?!.*[_.-]{2})[a-zA-Z0-9_-]+(?![^._-])$`", filter_input(INPUT_POST, "username")) ? filter_input(INPUT_POST, "username") : false;
+                // CHECK SI PAS DEJA UTILISÉ
+
+                $mail = preg_match("`(?xim)^(?=.*\.[A-Z]+$)(?=([[:alnum:]\.+_-]+)@(?1))(?!.*@.*@)(?!.*?@.*\.\d+$)(?!([[:punct:]]))(?!.*\.{2,})(?!.*(?2)@)(?!.*@(?2)).*`", filter_input(INPUT_POST, "mail")) ? filter_input(INPUT_POST, "mail", FILTER_VALIDATE_EMAIL) : false;
+                // CHECK SI PAS DEJA UTILISÉ
+                
+                $password = preg_match("`^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,100}$`" ,filter_input(INPUT_POST, "password")) ? filter_input(INPUT_POST, "password") : false;
+                $password2 = preg_match("`^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,100}$`" ,filter_input(INPUT_POST, "password2")) ? filter_input(INPUT_POST, "password2") : false;
+                $password = ($password === $password2) ? $password : false;
+                // HASH
+
+                break;
+        }        
+
         require '/Applications/MAMP/htdocs/Projet-RS-HETIC/Models/Users.php';
         require '/Applications/MAMP/htdocs/Projet-RS-HETIC/Views/register.php';
     }
+
+    private function checkYears($birthdate){
+        $limit = 18;
+        $age = (date('md') < date('md', strtotime($birthdate))) ? (date('Y') - date('Y', strtotime($birthdate)) - 1) : (date('Y') - date('Y', strtotime($birthdate)));
+        return ($limit <= $age) ? true : false;
+    }
+
 }
--- src/Router.php
+++ src/Router.php
@@ -7,27 +7,28 @@ use AuthController\LoginController;
 use AuthController\RegisterController;
 
 class Router {
-    private string $_methode;
+    private string $_method;
     private string $_page;
     private $_controller;
 
     public function __construct(){
-        $this->_methode = filter_input(INPUT_SERVER, "REQUEST_METHOD");
+        $this->_method = filter_input(INPUT_SERVER, "REQUEST_METHOD");
 
-        if ($this->_methode == "GET"){
-            if (filter_input(INPUT_GET, "p")){
-                $this->_page = filter_input(INPUT_GET, "p");
-            } else {
-                $this->_page = "login";
-            }
+        switch($this->_method){
+            case "GET":
+                $this->_page = filter_input(INPUT_GET, "p") ? filter_input(INPUT_GET, "p") : "login";
+                break;
+            case "POST":
+                $this->_page = filter_input(INPUT_GET, "p") ? filter_input(INPUT_GET, "p") : "login";
+                break;
         }
 
         switch($this->_page){
             case "login":
-                $this->_controller = new LoginController();
+                $this->_controller = new LoginController($this->_page, $this->_method);
                 break;
             case "register":
-                $this->_controller = new RegisterController();
+                $this->_controller = new RegisterController($this->_page, $this->_method);
                 break;
             default:
                 echo("404");
