3dc14d7f0a504b915aab73a5464b3bf3c9efefb3
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-25 21:48:57 +0200

    feat: search input now active

--- Views/templates/header.php
+++ Views/templates/header.php
@@ -1,4 +1,9 @@
 <?php
+if(isset($_POST["searchInput"])) {
+    $redirect = "Location: "."http://localhost/projet-rs-hetic/public/index.php?p=search&filter=".$_POST["searchInput"];
+    header($redirect);
+}
+
 
 ?>
 <head>
@@ -7,14 +12,14 @@
 
 <header>
     <a href="http://localhost/projet-rs-hetic/public/index.php?p=feed"><h1>Uni</h1></a>
-    <div class="inputSearch">
+    <form class="inputSearch" method="post">
         <label>
             <img src="../Views/assets/icons/searchIcon.svg" alt="Icone de recherche">
-            <input type="search" placeholder="Chercher un membre">
         </label>
-    </div>
+        <input type="search" name="searchInput" placeholder="Chercher un membre ou une page">
+    </form>
     <nav class="headerNav">
         <a href="#" class="headerCTA">Create</a>
-        <img class="userPdp" src="../Views/assets/imgs/users/picture/<?= $username ?? "default_picture.jpg" ?>" alt="Image de <?= $username ?? "anonyme" ?>">
+        <img class="userPdp" src="../Views/assets/imgs/users/picture/<?="default_picture.jpg"?>" alt="Image de <?= $username ?? "anonyme" ?>">
     </nav>
 </header>
