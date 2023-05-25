f07a50e8f38928c5096b2eac96d28ca3b54356f0
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-25 21:47:12 +0200

    feat: search semantics & imports

---
+++ Views/search.php
@@ -0,0 +1,65 @@
+<?php
+
+require ('../Controllers/SearchController.php');
+use Search\SearchController;
+
+$searchFilter = $_GET["filter"];
+$searchController = new SearchController();
+
+$renderData = $searchController->getAccounts($searchFilter);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../Views/styles/style.css">
+    <link rel="stylesheet" type="text/css" href="../Views/styles/search.css">
+
+    <title>Search</title>
+</head>
+
+<body>
+    <?php include '../Views/templates/header.php'; ?>
+    <main>
+        <?php require_once("../Views/templates/side_profile.php"); ?>
+        <section id="searchFeed">
+            <div class="accountRadio">
+                <div class="labelAccount">
+                    <label for="userType">Utilisateurs</label>
+                    <input type="radio" name="accountType" id="userType" value="users" checked>
+                </div>
+                <div class="labelAccount">
+                    <label for="pageType">Pages</label>
+                    <input type="radio" name="accountType" id="pageType" value="pages">
+                </div>
+
+            </div>
+            <div id="users">
+                <?php if(!$renderData["users"]): ?>
+                    <span>Aucun utilisateur n'a ce nom. Vérifiez dans les pages.</span>
+                <?php endif;?>
+
+                <?php foreach ($renderData["users"] as $user): ?>
+                <div class="userCard">
+                    <div>
+                        <h2><?= $user["name"] ?></h2>
+                        <span>@<?= $user["at"]?></span>
+                    </div>
+                    <a href="http://localhost/projet-rs-hetic/public/index.php?p=profile&profile_id=<?=$user["id"]?>" class="username"> Voir le profil</a>
+                </div>
+                <?php endforeach; ?>
+            </div>
+            <div id="pages">
+                <?php if(!$renderData["pages"]): ?>
+                    <span>Aucune page n'a ce nom. Vérifiez dans les utilisateurs.</span>
+                <?php endif;?>
+
+            </div>
+        </section>
+    </main>
+</body>
+<script src="../Views/script/search.js"></script>
