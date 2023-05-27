1a3568853cb09e1ee273771ac9b635c889582e2d
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-05-27 10:17:52 +0200

    fix: remove notifs link for the feed

--- Views/templates/side_profile.php
+++ Views/templates/side_profile.php
@@ -4,19 +4,18 @@
 
 <aside>
     <div class="profile">
-        <img class ="pp"src="<?= "https://www.liberation.fr/resizer/4aGj4IanoRaHCqrPJx-tLrl0uYY=/1024x0/filters:format(jpg):quality(70):focal(2395x2721:2405x2731)/cloudfront-eu-central-1.images.arcpublishing.com/liberation/2GMZBJOWNNBNVHMMTL3XJOIV7I.jpg" ?>"
-            alt="PP" />
+        <img class ="pp"src="../Views/assets/imgs/users/picture/default_picture.jpg" alt="User profil pic">
         <span>@<?= $username ?><span>
     </div>
     <nav>
         <ul>
             <li><a href="index.php?p=profile&profile_id=<?= $_COOKIE['uniCookieUserID'] ?>">Profil</a></li>
-            <li><a href="http://">Notifications</a></li>
-            <li><a href="index.php?p=friends">Friends</a></li>
+            <li><a href="index.php?p=feed">Feed</a></li>
+            <li><a href="index.php?p=friends">Mes Amis</a></li>
             <li><a href="index.php?p=pages">Pages</a></li>
-            <li><a href="index.php?p=userOptions">Options d'utilisateur</a></li>
-            <li><a href="index.php?p=logout&type=device">Deconnexion</a></li>
-            <li><a href="index.php?p=logout&type=allDevice">Deconnexion global</a></li>
+            <li><a href="index.php?p=userOptions">Options</a></li>
+            <li><a href="index.php?p=logout&type=device">Déconnexion</a></li>
+            <li><a href="index.php?p=logout&type=allDevice">Déconnexion global</a></li>
         </ul>
     </nav>
 
